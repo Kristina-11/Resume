f174a151b91b39f9659e6e0fa3460837cfac2168
Kristina <tina91kris11@gmail.com>
2021-01-22 13:25:20 +0100

    English data for About page. Added pdf resume and icons for social media references.

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,7 +1,10 @@
 import { useContext } from 'react';
 import { LanguageContext } from '../../context/LanguageContext';
 import src from '../../img/about.jpg';
+import email from '../../img/email.svg';
+import linkedIn from '../../img/linedin.png';
 import Picture from '../reusable/Picture';
+import resume from '../../pdf/Resume.pdf';
 
 const AboutMe = () => { 
     const { lang } = useContext(LanguageContext);
@@ -22,7 +25,27 @@ const AboutMe = () => {
                         &bull; Intuition
                     </span>
                     <div className="summary">
-                        
+                        <p>Motivated web developer with a year of experience with .NET framework and many moons experience as a freelancer. Diagnosed and solved some issues ( technical and personal :D), built a few websites as well as small projects.</p>
+                        <p>Determined to constantly learn and develop.</p>
+                    </div>
+                    <div className="links">
+                        <div className="links-text">
+                            If you are in need of web developer you can contact me via Email or LinkedIn:
+                        </div>
+                        <div className="email">
+                            <img src={email} width='20px' alt='email icon' />
+                            <span>kristina.jovanovic.code@gmail.com</span>
+                        </div>
+                        <div className="linked-in">
+                            <a href='www.linkedin.com/in/kristina91jovanovic11'>
+                                <img src={linkedIn} width='20px' alt='linkedIn icon' />
+                            </a>
+                            <span>www.linkedin.com/in/kristina91jovanovic11</span>
+                        </div>
+                        <div className="resume">
+                            <a href={resume} target="_blank">Resume</a>
+                            <p>PDF version resume</p>
+                        </div>
                     </div>
                 </div> : 
                 <div className="about">
@@ -35,6 +58,10 @@ const AboutMe = () => {
                         &bull; Istrajnost
                         &bull; Intuicija
                     </span>
+                    <div className="summary">
+                        <p>Motivated web developer with a year of experience with .NET framework and many moons experience as a freelancer. Diagnosed and solved some issues ( technical and personal :D), built a few websites as well as small projects.</p>
+                        <p>Determined to constantly learn and develop.</p>
+                    </div>
                 </div>
             }
         </div>
