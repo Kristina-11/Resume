060310b330da5375ea5d7c8ec0a3cd42837c9279
Kristina <tina91kris11@gmail.com>
2021-02-01 17:20:23 +0100

    Navigation button for small devices and click events done.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,51 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { LanguageContext } from "../context/LanguageContext";
+
+import dropdownImg from "../img/dropdown.png";
 
 const Nav = () => {
     const { lang, changeLanguage } = useContext(LanguageContext);
 
+    const [ dropdown, setDropdown ] = useState(false);
+    
+    const handleDropdown = (e) => {
+        setDropdown(!dropdown);
+    }
+
+    useEffect(() => {
+        const dropdownElement = document.querySelector('.nav-left');
+
+        if (dropdown) {
+            dropdownElement.classList.add('nav-show')
+            dropdownElement.classList.remove('nav-hide')
+        } else {
+            dropdownElement.classList.add('nav-hide')
+            dropdownElement.classList.remove('nav-show')
+        }
+
+        window.onclick = (e) => {
+            if (!e.target.matches('.nav-dropdown') && dropdown === true){
+                setDropdown(false)
+            }
+        }
+
+    },[dropdown])
+
     return ( 
         <div className="nav">
+            <div className="nav-dropdown" onClick={handleDropdown} >
+                <img src={dropdownImg} alt="Dropdown menu" />
+            </div>
             { lang === 'en' ? 
-                <div className="nav-left">
+                <div className="nav-left nav-hide">
                     <Link to='/'>About</Link>
                     <Link to='/skills'>Skills</Link>
                     <Link to='/portfolio'>Portfolio</Link>
                     <Link to='/work'>Experience&Education</Link>
                     <Link to='/interests'>Interests</Link>
                 </div> : 
-                <div className="nav-left">
+                <div className="nav-left nav-hide">
                     <Link to='/'>Info</Link>
                     <Link to='/skills'>Veštine</Link>
                     <Link to='/portfolio'>Portfolio</Link>
